d52872b100ab8860bd0bee169d8a6d87a33816c3
Ikhide Augustine <augustineimoleme@gmail.com>
2023-04-18 10:53:41 +0100

    Fift commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('products/<slug:slug>/', views.ProductDetailView.as_view(), name='product'),
-    path('checkout', views.checkout, name='checkout'),
+    path('add-to-cart/<slug:slug>/', views.AddToCartView.as_view(), name='add-to-cart'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from .models import Item, OrderItem, Order
+from django.contrib import messages
+from django.utils import timezone
+from django.views import View
 
 
 class HomeView(ListView):
@@ -13,6 +17,39 @@ class ProductDetailView(DetailView):
     template_name = 'product.html'
     context_object_name = 'product'
 
-def checkout(request):
-    return render(request, 'checkout.html')
+
+class AddToCartView(View):
+    def get(self, request, slug):
+        item = get_object_or_404(Item, slug=slug)
+        order_item, created = OrderItem.objects.get_or_create(item=item, user=request.user, ordered=False)
+        order_qs = Order.objects.filter(user=request.user, ordered=False)
+        if order_qs.exists():
+            order = order_qs[0]
+            if order.items.filter(item__slug=item.slug):
+                order_item.quantity += 1
+                order_item.save()
+                messages.success(request, f"{item.title}'s quantity was successfully updated in cart")
+                return redirect('product', slug=slug)
+            else:
+                order.items.add(order_item)
+                order.save()
+                messages.success(request, f"{item.title} was successfully added to cart")
+                return redirect('product', slug=slug)
+            
+        else:
+            ordered_date = timezone.now()
+            order = Order.objects.create(user=request.user, ordered=False, ordered_date=ordered_date)
+            order.items.add(order_item)
+            order.save()
+            messages.success(request, f"{item.title} was successfully added to cart")
+            return redirect('product', slug=slug)
+
+    def post(self, request, slug):
+        return self.get(request, slug)
+        
+        
+        
+    
+    
+    
 
